9e8acaef686a4f1a702944dbdcc06fb17c21132a
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-22 13:58:12 +0100

    scritto procedura del progetto

---
+++ email/JS/index.js
@@ -0,0 +1,6 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+//  stampa un messaggio appropriato sull’esito del controllo.
+
+// chiedere ad utente email
+// verificare se dentro array è inserito la sua email
+// far vedere output ad utente
