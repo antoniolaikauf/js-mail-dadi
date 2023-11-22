b3a71e8e582cd3940eb9c400f6eaa602fbe661e8
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-22 18:04:36 +0100

    fixed : tolto for loop dentro eventi verificaUser

--- email/JS/index.js
+++ email/JS/index.js
@@ -9,21 +9,20 @@ console.log(emailUser);
 // creare variabile con pulsante 
 const verificaUser= document.getElementById("verifica");
 
-
-// evento per vedere se presente in lista
+// finizio evento 
 verificaUser.addEventListener("click", function () {
     let valoreEmail = emailUser.value;
     let risultato;
+    // creazione lista 
     const arrayEmail=["antonio.pesenti@gmail.com", "federico.astori@gmail.com","luca.astori@gmail.com"];
-    for (let i = 0; i < arrayEmail.length; i++) {
         // controllo se email presente in lista 
         if (valoreEmail ===  arrayEmail[0] || valoreEmail ===  arrayEmail[1] || valoreEmail === arrayEmail[2] ) {
             risultato="puoi entrare"
         } else {
             risultato="non puoi entrare"
         }
-    }
     document.getElementById("output").innerHTML=risultato;
 })
+
  
 
