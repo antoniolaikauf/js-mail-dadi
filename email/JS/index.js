44bd21d6a965b725da5020b63a2ca8111d5bf957
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-22 14:44:31 +0100

    progetto email finito

--- email/JS/index.js
+++ email/JS/index.js
@@ -1,23 +1,28 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
 //  stampa un messaggio appropriato sull’esito del controllo.
 
-// chiedere ad utente email
-// verificare se dentro array è inserito la sua email
-// far vedere output ad utente
 
+
+// chiedere ad utente email
 const emailUser = document.getElementById("dati-utente");
 console.log(emailUser);
-
+// creare variabile con pulsante 
 const verificaUser= document.getElementById("verifica");
 
-const arrayEmail=["antonio.laikauf00@gmail.com","marco.pesenti@gmail.com","luca.astori@gmail.com"];
-
-
-
 verificaUser.addEventListener("click", function(){
+    const arrayEmail=["antonio.laikauf00@gmail.com","marco.pesenti@gmail.com","luca.astori@gmail.com"];
+    let valoreEmail = emailUser.value;
     for (let i = 0; i < arrayEmail.length; i++) {
         // console.log(arrayEmail[i]);
-        let valoreEmail = emailUser.value;
-        console.log(valoreEmail);
+        // verificare se dentro array è inserito la sua email
+        if (valoreEmail === "antonio.laikauf00@gmail.com" || valoreEmail ==="marco.pesenti@gmail.com" || valoreEmail === "luca.astori@gmail.com"  ) {
+            // far vedere output ad utente
+            document.getElementById("output").innerHTML="puoi entrare";
+        } else {
+            // far vedere output ad utente
+            document.getElementById("output").innerHTML="non puoi entrare";
+        }
     }
+
 })
+
