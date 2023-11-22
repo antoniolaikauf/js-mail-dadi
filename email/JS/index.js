c8503f56922fc3473f6110a8251c39469b333c41
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-22 18:13:08 +0100

    add : for loop dentro envento verificaUser

--- email/JS/index.js
+++ email/JS/index.js
@@ -9,18 +9,18 @@ console.log(emailUser);
 // creare variabile con pulsante 
 const verificaUser= document.getElementById("verifica");
 
-// finizio evento 
+// inizio evento 
 verificaUser.addEventListener("click", function () {
     let valoreEmail = emailUser.value;
-    let risultato;
+    let risultato="non entri";
     // creazione lista 
     const arrayEmail=["antonio.pesenti@gmail.com", "federico.astori@gmail.com","luca.astori@gmail.com"];
-        // controllo se email presente in lista 
-        if (valoreEmail ===  arrayEmail[0] || valoreEmail ===  arrayEmail[1] || valoreEmail === arrayEmail[2] ) {
+        // controllo se email presente in lista con for loop
+       for (let i = 0; i < arrayEmail.length; i++) {
+        if (valoreEmail ===  arrayEmail[i] ) {
             risultato="puoi entrare"
-        } else {
-            risultato="non puoi entrare"
-        }
+        } 
+       }
     document.getElementById("output").innerHTML=risultato;
 })
 
